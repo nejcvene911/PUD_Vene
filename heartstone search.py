40495e5ee4f11cde04b4cbddcbb185ee71ecb27f
nejcvene911 <63786101+nejcvene911@users.noreply.github.com>
2021-02-02 12:05:57 +0100

    Add files via upload

---
+++ heartstone search.py
@@ -0,0 +1,15 @@
+import http.client
+
+conn = http.client.HTTPSConnection("omgvamp-hearthstone-v1.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "75791661f4mshe18cbffd10a10f0p19b179jsn7e232b78597a",
+    'x-rapidapi-host': "omgvamp-hearthstone-v1.p.rapidapi.com"
+    }
+
+conn.request("GET", "/cards/search/%7Bname%7D", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
